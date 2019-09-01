b7dc39bb385331be016af6329610cb13bc41682e
Pavel Ilin <pavel_ilin@yahoo.com>
2019-09-01 22:58:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,10 @@ def reverse_each_word (words)
   words.each{|word| word.reverse!} 
   words.join(" ")
 end
+
+
+def reverse_each_word (words)
+  words = words.split()
+  words.each{|word| word.reverse!} 
+  words.join(" ")
+end
